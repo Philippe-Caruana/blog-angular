cd1da975aeb4aa8fd2171e2d72e8f24e37f4728a
Philippe Caruana <philippecaruana@free.fr>
2019-01-28 17:41:00 +0100

    Insert data into an object array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  	selector: 'app-root',
+  	templateUrl: './app.component.html',
+  	styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'blog-angular';
+
+  	postsList = [
+
+  		{
+  			title: "Mon premier post",
+  			content: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",
+  		},
+
+  		{
+  			title: "Mon deuxième post",
+  			content: "Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.",
+  		},
+
+  		{
+  			title: "Mon troisième post",
+  			content: "Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. ",
+  		}
+
+  	]
+
 }

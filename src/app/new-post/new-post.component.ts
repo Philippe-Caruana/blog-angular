b449a6383f689ac5f8832f7cb922e0959501f6cf
Philippe Caruana <philippecaruana@free.fr>
2019-03-20 13:50:51 +0100

    Add a Reactive Form in order to add a new post

--- src/app/new-post/new-post.component.ts
+++ src/app/new-post/new-post.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PostsService } from '../services/posts.service';
+import { Post } from '../models/Post.model';
 
 @Component({
-  selector: 'app-new-post',
-  templateUrl: './new-post.component.html',
-  styleUrls: ['./new-post.component.css']
+	selector: 'app-new-post',
+	templateUrl: './new-post.component.html',
+	styleUrls: ['./new-post.component.css']
 })
 export class NewPostComponent implements OnInit {
 
-  constructor() { }
+	postForm: FormGroup;
+	errorMessage: string;
 
-  ngOnInit() {
-  }
+	constructor(private formBuilder: FormBuilder,
+				private postsService: PostsService, 
+				private router: Router) { 
+		
+		
+	}
+
+	ngOnInit() {
+		this.initForm();
+	}
+
+	initForm() {
+		this.postForm = this.formBuilder.group({
+			title: ['', [Validators.required]],
+			content: ['', [Validators.required]]
+		});
+	}
+
+	onSavePost() {
+		const title = this.postForm.get('title').value;
+		const content = this.postForm.get('content').value;
+		
+		const newPost = new Post(title, content);
+
+		this.postsService.createNewPost(newPost);
+	
+		this.router.navigate(['/posts']);
+	}
 
 }

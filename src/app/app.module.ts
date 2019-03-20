514109a2d886d0ea309d90c18c481aa25f56d474
Philippe Caruana <philippecaruana@free.fr>
2019-03-20 13:16:12 +0100

    Add routing into the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import localeFr from '@angular/common/locales/fr';
 
 registerLocaleData(localeFr, 'en');
@@ -10,17 +14,33 @@ import { PostListComponent } from './post-list/post-list.component';
 import { PostListItemComponent } from './post-list-item/post-list-item.component';
 import { NewPostComponent } from './new-post/new-post.component';
 
+import { PostsService } from './services/posts.service';
+
+const appRoutes: Routes = [
+	{ path: 'posts', component: PostListComponent },
+	{ path: 'new', component: NewPostComponent },
+	{ path: '', redirectTo: 'posts', pathMatch: 'full' },
+	{ path: '**', redirectTo: 'posts' }
+]
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostListComponent,
-    PostListItemComponent,
-    NewPostComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		PostListComponent,
+		PostListItemComponent,
+		NewPostComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		ReactiveFormsModule,
+		HttpClientModule,
+		FontAwesomeModule,
+		RouterModule.forRoot(appRoutes)
+	],
+	providers: [
+		PostsService
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }

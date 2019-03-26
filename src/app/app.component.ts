3a5e8590982bf99ab8ca888dbae54f9ab0fa3d6b
Philippe Caruana <philippecaruana@free.fr>
2019-03-26 14:56:53 +0100

    Update AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,14 @@ import * as firebase from 'firebase';
 export class AppComponent {
 
 	constructor() {
-		
+
 		/* 
 			To get your Firebase config, go to : https://console.firebase.google.com/
-			- Add a project
-			- Add Firebase to your web application by clicking on the </> icon
-			- Copy and paste the config variable here
-			- Add a realtime database to your project : you can choose between the test and the locked mode
+			-> 	Add a project
+			-> 	Add Firebase to your web application by clicking on the </> icon
+			->	Copy and paste the config variable here
+			-> 	Add a realtime database to your project : you can choose between the test and the locked mode. 
+				Choose the test mode to test the project, the locked mode is used in production.
 		*/
 
 		// Initialize Firebase

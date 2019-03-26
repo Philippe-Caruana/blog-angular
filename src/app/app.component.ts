68b85d9811a45b00a06c0a74399b35b41ebc7a52
Philippe Caruana <philippecaruana@free.fr>
2019-03-26 14:36:44 +0100

    Update AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,24 @@ import * as firebase from 'firebase';
 export class AppComponent {
 
 	constructor() {
+		
+		/* 
+			To get your Firebase config, go to : https://console.firebase.google.com/
+			- Add a project
+			- Add Firebase to your web application by clicking on the </> icon
+			- Copy and paste the config variable here
+			- Add a realtime database to your project : you can choose between the test and the locked mode
+		*/
+
+		// Initialize Firebase
+
 		var config = {
-			apiKey: "AIzaSyA0_26UrG9CNeaTdSXGCzjvlGJf1ryd9G4",
-			authDomain: "blog-39015.firebaseapp.com",
-			databaseURL: "https://blog-39015.firebaseio.com",
-			projectId: "blog-39015",
-			storageBucket: "blog-39015.appspot.com",
-			messagingSenderId: "365584144904"
+			apiKey: "apiKey",
+			authDomain: "authDomain",
+			databaseURL: "databaseURL",
+			projectId: "projectId",
+			storageBucket: "storageBucket",
+			messagingSenderId: "messagingSenderId"
 		};
 
 		firebase.initializeApp(config);
